48d8816a301e2b1c74dc9136cdf2ed371244ada0
PavelKlimovich <pavel@kernl.fr>
2022-10-26 00:20:42 +0200

    snowtricks-006: Add Front style.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function index(TrickRepository $repo): Response
     {
-        return $this->render('index.html.twig');
+        $tricks = $repo->findAll();
+
+        return $this->render('index.html.twig', [
+            'tricks' => $tricks
+        ]);
+    }
+
+    #[Route('/trick/{id}', name: 'show')]
+    public function show(TrickRepository $repo, $id): Response
+    {
+        $trick = $repo->find($id);
+
+        return $this->render('show.html.twig', [
+            'trick' => $trick
+        ]);
     }
 }
