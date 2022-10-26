935975adaf8ca8f019b76628c780c5dca87236bb
Pavel Klimovich <pavelklimovich@hotmail.fr>
2022-10-26 11:39:00 +0200

    docker

--- migrations/Version20221019103000.php
+++ migrations/Version20221019103000.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221017211549 extends AbstractMigration
+final class Version20221019103000 extends AbstractMigration
 {
     public function getDescription(): string
     {
