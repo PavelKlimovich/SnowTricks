31e838f71fd106a093a7569bdc992e75a2f58acf
PavelKlimovich <pavel@kernl.fr>
2022-11-14 23:44:26 +0100

    snowtricks-dev Fixe.

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,15 +5,24 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixture extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher) {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
+        $plaintextPassword = 'password';
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plaintextPassword);
         $user->setUsername("pklim")
             ->setEmail("pavel@kernl.fr")
-            ->setPassword("password")
+            ->setPassword($hashedPassword)
             ->setRoles(['ROLE_USER'])
             ->setIsVerified(1)
             ->setCreatedAt(new \DateTime());
