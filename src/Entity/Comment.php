14cce4a420a931cfbfa913f26ed7f6109581e06e
Pavel Klimovich <pavelklimovich@hotmail.fr>
2022-12-07 11:05:57 +0100

    snowtricks-009: Fix edit validation.

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
 {
@@ -23,6 +23,7 @@ class Comment
     private ?Trick $trick = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank()]
     private ?string $content = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
