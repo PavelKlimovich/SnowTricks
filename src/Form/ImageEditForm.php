dec72ad74396ce4820f96791bf4cada0d27e691c
PavelKlimovich <pavel@kernl.fr>
2023-01-18 01:30:14 +0100

    snowtricks-011: Add CRUD to Image and Video entities.

---
+++ src/Form/ImageEditForm.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image as ImageClass;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ImageEditForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('hidden', HiddenType::class, [
+                    'data' => '',
+                ])
+                ->add('trick_id', HiddenType::class, [
+                    'data' => '',
+                ])
+                ->add('image', FileType::class, [
+                        'mapped' => false,
+                        'required' => true,
+                        'data_class' => null,
+                        'attr' => [
+                            'accept' => "image/*"
+                        ],
+                    ]);
+    }
+ 
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ImageClass::class,
+            // enable/disable CSRF protection for this form
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'task_item',
+        ]);
+    }
+}
